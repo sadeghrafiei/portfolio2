f538bdfacad2c06b13ffe27982121ae6ed4870b2
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-02-27 16:45:14 +0330

    add img and data

---
+++ src/data/projectsData.js
@@ -0,0 +1,47 @@
+import image1 from './assets/img/project1.jpg'
+import image2 from './assets/img/project-2.png'
+import image3 from './assets/img/project-3.png'
+import image4 from './assets/img/projet-4.png'
+export const projectsData = [
+  {
+    id: 1,
+    title: "Twitter",
+    date: "Feb 2021",
+    languages: ["React", "firebase", "Material-Ui"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: image1,
+    link: "https://sr-twitter-ui.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Netflix",
+    date: "Jan 2021",
+    languages: ["React.js" , "netflix-api"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: image2,
+    link: "https://sr-react-netflix-clone.herokuapp.com/",
+  },
+  {
+    id: 3,
+    title: "Shopping Cart",
+    date: "March 2020",
+    languages: ["React" , "Redux" , "node.js"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: image3,
+    link: "https://sadegh-rafiei-shop.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Portfolio",
+    date: "Feb 2021",
+    languages: ["React", "Sass"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: image4,
+    link: "https://sadeghrafiei.netlify.app/",
+    
+  },
+];
