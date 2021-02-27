6f953bb22305ab65ae0fc4574da2ea39ba136959
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-02-27 16:44:38 +0330

    step1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import { Project1, Project2, Project3, Project4 } from './pages/Project'
+
+
+function App() {
+  return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/project-1" component={Project1} />
+        <Route exact path="/project-2" component={Project2} />
+        <Route exact path="/project-3" component={Project3} />
+        <Route exact path="/project-4" component={Project4} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch> 
+  )
+}
+
+export default App
